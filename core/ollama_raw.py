933947de6a79efe349e47c8c605434631a5a0747
U CHANDAN SAI PRATHYUSH <ucsp09@gmail.com>
2026-01-16 03:29:40 +0530

    added first working version with no message history, no streaming

---
+++ core/ollama_raw.py
@@ -0,0 +1,30 @@
+import aiohttp
+from config import constants
+
+async def callOllamaChatAPIWithMessagesAndNoStream(session: aiohttp.ClientSession, model: str, messages: list):
+    try:
+        url = f"{constants.OLLAMA_HOST}:{constants.OLLAMA_PORT}/api/chat"
+        headers = {
+            "Content-Type": "application/json"
+        }
+        payload = {
+            "model": model,
+            "messages": messages,
+            "max_tokens": constants.OLLAMA_MAX_RESPONSE_TOKENS,
+            "stream": False
+        }
+        print("Making request to ollama generate API ...")
+        print(f"curl -X POST {url} -d \'{payload}\' -H \"Content-Type: application/json\"")
+        async with session.post(url=url, headers=headers, json=payload) as response:
+            if response.status == 200:
+                print("Ollama Generate API is successful !!!")
+                json_response = await response.json()
+                print(json_response)
+                return json_response, None
+            else:
+                text_response = await response.text()
+                print(text_response)
+                return None, Exception(f"Ollama Generate API Failed with status code: {response.status}")
+    except Exception as e:
+        print(f"Error occured while making request to ollama generate API: {e}")
+        return None, e
